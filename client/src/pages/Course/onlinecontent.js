c46493ce27df04ede8157cce0ac293ff6427a427
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-16 22:43:18 +0530

    Update and rename testcontent.js to onlinecontent.js

---
+++ client/src/pages/Course/onlinecontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import OnlineContainerCourses from "./OnlineContainerCourses.js";
+
+const onlinecontent=()=> {
+  return (
+    <div >
+      <OnlineContainerCourses />
+    </div>
+  );
+}
+
+export default onlinecontent;
