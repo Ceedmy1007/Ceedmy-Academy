049c9a3d037541bea631193417b04dc009626099
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-16 18:47:04 +0530

    Create repeaterscontent.js

---
+++ client/src/pages/Course/repeaterscontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RepeatersContainerCourses from "./RepeatersContainerCourses.js";
+
+const repeaterscontent=()=> {
+  return (
+    <div >
+      <RepeatersContainerCourses />
+    </div>
+  );
+}
+
+export default repeaterscontent;
