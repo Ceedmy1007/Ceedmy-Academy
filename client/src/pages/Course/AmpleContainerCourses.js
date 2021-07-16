8612262762a67ab48d68f35726f722724bcf7588
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-16 15:21:58 +0530

    Create AmpleContainerCourses.js

---
+++ client/src/pages/Course/AmpleContainerCourses.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Containercourses = () => {
+  return (
+      <>
+     
+    <div className="container mt-2"
+         style={{
+              marginRight: "15%",
+              marginLeft: "15%",
+         }}
+    >
+      <div className="flexClass">
+        <nav aria-label="breadcrumb">
+         
+          <h2
+            style={{
+              fontFamily: "helvetica",
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: "30px",
+            }}
+          >
+            NEET Courses
+          </h2>
+          <br>
+          <h3
+            style={{
+              font-family: "verdana",     
+              textAlign: "center",
+              fontSize: "23px",
+            }}
+          >
+            AMPLE Program (NEET + AIIMS + JIPMER + OLYMPIADS)
+          </h2>
+        </nav>
+        <br><br>
+        <ul>
+              <li>
+     </div>
+   </div>
+ </>
+  );
+};
+export default Containercourses;
