65895f7b019d1642df5c5d3125df394c99be142f
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-16 18:30:10 +0530

    Create TestContainerCourses.js

---
+++ client/src/pages/Course/TestContainerCourses.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const TestContainerCourses = () => {
+  return (
+      <>
+     
+    <div className="container mt-5 mx-5"
+    >
+      <div className="flexClass">
+        <nav aria-label="breadcrumb">
+         
+          <h2
+            style={{
+              fontFamily: "helvetica",
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: "large",
+            }}
+          >
+            NEET Courses
+          </h2>
+          <br />
+          <h2
+            style={{
+              fontFamily: "verdana",     
+              textAlign: "center",
+              fontSize: "medium",
+            }}
+          >
+            ALL INDIA TEST SERIES (ONLINE/OFFLINE)
+          </h2>
+        </nav>
+        <br />
+              <p style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>This Course is designed for those aspirants who have studied the topics themselves and want to check their preparation level and compete with Top Rankers on an All India basis for Competitive Exams. The students prepared for the test as scheduled provided to them at the time of admission. Students are provided with a detailed analysis of the test.</p>
+     </div>
+   </div>
+ </>
+  );
+};
+export default TestContainerCourses;
