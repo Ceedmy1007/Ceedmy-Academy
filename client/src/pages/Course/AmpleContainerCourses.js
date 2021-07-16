bfaa54f6efef7847ead914635583d76a215c6294
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-16 15:28:42 +0530

    Update AmpleContainerCourses.js

--- client/src/pages/Course/AmpleContainerCourses.js
+++ client/src/pages/Course/AmpleContainerCourses.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Containercourses = () => {
+const AmpleContainercourses = () => {
   return (
       <>
      
@@ -36,7 +36,15 @@ const Containercourses = () => {
         </nav>
         <br><br>
         <ul>
-              <li>
+              <li style={{marginRight: "15px", marginLeft: "15px"}}>This course is designed for students to make them fundamentally strong in physics, chemistry and biology with analytical skills to solve problems in competitive exams.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px"}}>This will cover the whole syllabus in depth.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px"}}>Regular Topic Wise Tests (TWTs) are conducted (Online/ Offline) to ensure that the students have fundamentally understood the topic well and the conceptual knowledge of the topic is developed.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px"}}>Major test with cumulative portion is taken periodically to give students a real-time understanding of the difficulty level, time management, the diverse set of questions and patterns and also ranking at an All India level on NEET/AIIMS/JIPMER.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px"}}>Online tests are given so that students get real-time exposure to the new pattern of NEET testing from the beginning.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px"}}>All India Test Series (AITS) is a series of full-length tests conducted after course completion to give the student immense practice for the NEET/AIIMS/JIPMER and also to know their national-level performance.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px"}}>Regular feedback is taken from the students to identify actions to be taken to improve & enhance their performance.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px"}}>A detailed analysis report is given after every test.</li>
+        </ul>
      </div>
    </div>
  </>
