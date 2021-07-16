293181ae8ab0899738b22acce4d23e92dd38302c
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-16 18:19:02 +0530

    Create RepeatersContainerCourses.js

---
+++ client/src/pages/Course/RepeatersContainerCourses.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const RepeatersContainerCourses = () => {
+  return (
+      <>
+     
+    <div className="container mt-5 mx-5"
+    >
+      <div className="flexClass">
+        <nav aria-label="breadcrumb">
+         
+          <h2
+            style={{
+              fontFamily: "helvetica",
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: "large",
+            }}
+          >
+            NEET Courses
+          </h2>
+          <br />
+          <h2
+            style={{
+              fontFamily: "verdana",     
+              textAlign: "center",
+              fontSize: "medium",
+            }}
+          >
+            Repeaters Program (NEET + AIIMS + JIPMER)
+          </h2>
+        </nav>
+        <br />
+        <p style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>●	In this program, we are providing knowledge that helps students in weak areas of his/her previous year’s preparation. This program will cover the entire syllabus of NEET/AIIMS/JIPMER.</p>      
+     </div>
+   </div>
+ </>
+  );
+};
+export default RepeatersContainerCourses;
