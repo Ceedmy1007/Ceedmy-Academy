b955762c46bd4875124e0720c84dfca4c60a9b5c
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-16 18:43:43 +0530

    Create crashcontent.js

---
+++ client/src/pages/Course/crashcontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CrashContainerCourses from "./CrashContainerCourses.js";
+
+const crashcontent=()=> {
+  return (
+    <div >
+      <CrashContainerCourses />
+    </div>
+  );
+}
+
+export default crashcontent;
