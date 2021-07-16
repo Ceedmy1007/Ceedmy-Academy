653f39bc9f731d721c7aecf8afdcb09a2f572a96
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-16 17:29:37 +0530

    Create binarycontent.js

---
+++ client/src/pages/Course/binarycontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BinaryContainerCourses from "./BinaryContainerCourses.js";
+
+const binarycontent=()=> {
+  return (
+    <div >
+      <BinaryContainerCourses />
+    </div>
+  );
+}
+
+export default binarycontent;
