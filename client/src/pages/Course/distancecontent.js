1b7b68cd6c6fc20717804aa985cf10520cc28f93
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-16 18:44:58 +0530

    Create distancecontent.js

---
+++ client/src/pages/Course/distancecontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DistanceContainerCourses from "./DistanceContainerCourses.js";
+
+const distancecontent=()=> {
+  return (
+    <div >
+      <DistanceContainerCourses />
+    </div>
+  );
+}
+
+export default distancecontent;
