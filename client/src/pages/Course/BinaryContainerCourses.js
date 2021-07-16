a17e9056d742038ee7c956dd4466e5b68d1ad890
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-16 17:28:03 +0530

    Create BinaryContainerCourses.js

---
+++ client/src/pages/Course/BinaryContainerCourses.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const BinaryContainerCourses = () => {
+  return (
+      <>
+     
+    <div className="container mt-4 mx-5"
+    >
+      <div className="flexClass">
+        <nav aria-label="breadcrumb">
+         
+          <h2
+            style={{
+              fontFamily: "helvetica",
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: "large",
+            }}
+          >
+            NEET Courses
+          </h2>
+          <br />
+          <h2
+            style={{
+              fontFamily: "verdana",     
+              textAlign: "center",
+              fontSize: "medium",
+            }}
+          >
+            Binary Program (NEET + AIIMS + JIPMER)
+          </h2>
+        </nav>
+        <br />
+        <ul>
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>This course is designed to cover the depth and breadth of competitive examinations preparation.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>Practice test papers that help in the preparation of competitive exams are also provided.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>All India Test Series (AITS) is a series of full-length tests conducted after course completion to give the student immense practice for the NEET/AIIMS/JIPMER and also to know their national-level performance.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>Regular feedback is taken from the students to identify actions to be taken to improve & enhance their performance.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>A detailed analysis report is given after every test.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>Our faculty is a team of highly experienced doctorates and top academicians from their respective fields who excel at competitive exam preparation.</li>
+        </ul>
+     </div>
+   </div>
+ </>
+  );
+};
+export default BinaryContainerCourses;
