7581a260c4aa106f8262720a7e21028da0e3c03c
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-17 10:39:20 +0530

    Update CrashContainerCourses.js

--- client/src/pages/Course/CrashContainerCourses.js
+++ client/src/pages/Course/CrashContainerCourses.js
@@ -35,7 +35,9 @@ const CrashContainerCourses = () => {
               <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>This program focuses on covering important topics in a systematic way and simultaneously trains students to tackle the entrance examination to improve their expected All India Rank.</li>
               <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>The entire syllabus will be  covered in the short period by the expert professors and it is discussed from the entrance exam point of view. To give the students a feel of the actual exams, they are also provided with the All India Test Series.</li>
               <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>This Course is designed to cover the entire syllabus of NEET in a short period.</li>
-
+              <br />
+              <hr style={{borderTop: "1px solid"}} />
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", listStyle: "none"}}>For more details :- kindly contact on mobile number <u style={{color: "black"}}>7900007174</u></li>  
         </ul>    
      </div>
    </div>
