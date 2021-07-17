d26a14c49d5436b99930feeb495f7fefccc33b93
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-17 19:27:27 +0530

    Update DistanceContainerCourses.js

--- client/src/pages/Course/DistanceContainerCourses.js
+++ client/src/pages/Course/DistanceContainerCourses.js
@@ -14,7 +14,7 @@ const DistanceContainerCourses = () => {
               fontFamily: "helvetica",
               textAlign: "center",
               fontWeight: "bold",
-              fontSize: "large",
+              fontSize: "30px",
             }}
           >
             NEET Courses
@@ -24,7 +24,7 @@ const DistanceContainerCourses = () => {
             style={{
               fontFamily: "verdana",     
               textAlign: "center",
-              fontSize: "medium",
+              fontSize: "22px",
             }}
           >
             Distance Learning (NEET)
@@ -32,14 +32,14 @@ const DistanceContainerCourses = () => {
         </nav>
         <br />
         <ul>
-              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>Students who are unable to join our regular classroom program can enroll in our distance learning program.</li>
-              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>This program offers students the flexibility to use time according to their requirement.</li>
-              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>This course is for all students who due to some reason could not join the regular classes.</li>
-              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>Our study material along with our Topic wise test & All India Test Series will be delivered to the students.</li>
-              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>We will provide online access on our portal.</li>
+              <li style={{fontSize: "20px", marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>Students who are unable to join our regular classroom program can enroll in our distance learning program.</li>
+              <li style={{fontSize: "20px", marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>This program offers students the flexibility to use time according to their requirement.</li>
+              <li style={{fontSize: "20px", marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>This course is for all students who due to some reason could not join the regular classes.</li>
+              <li style={{fontSize: "20px", marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>Our study material along with our Topic wise test & All India Test Series will be delivered to the students.</li>
+              <li style={{fontSize: "20px", marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>We will provide online access on our portal.</li>
               <br />
               <hr style={{borderTop: "1px solid"}} />
-              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", listStyle: "none"}}>For more details :- kindly contact on mobile number <u style={{color: "black"}}>7900007174</u></li>  
+              <li style={{fontSize: "20px", marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", listStyle: "none"}}>For more details :- kindly contact on mobile number <u style={{color: "black"}}>7900007174</u></li>  
         </ul>    
      </div>
    </div>
