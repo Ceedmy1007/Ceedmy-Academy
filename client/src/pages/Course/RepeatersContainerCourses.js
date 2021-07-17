b74b4c2af01bc1260787fd580d52924505c1c06d
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-17 10:41:35 +0530

    Update RepeatersContainerCourses.js

--- client/src/pages/Course/RepeatersContainerCourses.js
+++ client/src/pages/Course/RepeatersContainerCourses.js
@@ -33,6 +33,9 @@ const RepeatersContainerCourses = () => {
         <br />
         <ul>
               <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", listStyle: "none",}}>In this program, we are providing knowledge that helps students in weak areas of his/her previous year’s preparation. This program will cover the entire syllabus of NEET/AIIMS/JIPMER.</li>
+              <br />
+              <hr style={{borderTop: "1px solid"}} />
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", listStyle: "none"}}>For more details :- kindly contact on mobile number <u style={{color: "black"}}>7900007174</u></li>  
         </ul>          
      </div>
    </div>
