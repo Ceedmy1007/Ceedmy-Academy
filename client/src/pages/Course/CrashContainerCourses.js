4e756874b79ccc41c9634a4532e5d5de6b00364a
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-17 19:06:18 +0530

    Update CrashContainerCourses.js

--- client/src/pages/Course/CrashContainerCourses.js
+++ client/src/pages/Course/CrashContainerCourses.js
@@ -14,7 +14,7 @@ const CrashContainerCourses = () => {
               fontFamily: "helvetica",
               textAlign: "center",
               fontWeight: "bold",
-              fontSize: "large",
+              fontSize: "25px",
             }}
           >
             NEET Courses
@@ -32,12 +32,12 @@ const CrashContainerCourses = () => {
         </nav>
         <br />
         <ul>
-              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>This program focuses on covering important topics in a systematic way and simultaneously trains students to tackle the entrance examination to improve their expected All India Rank.</li>
-              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>The entire syllabus will be  covered in the short period by the expert professors and it is discussed from the entrance exam point of view. To give the students a feel of the actual exams, they are also provided with the All India Test Series.</li>
-              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif",}}>This Course is designed to cover the entire syllabus of NEET in a short period.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", fontSize: "20px"}}>This program focuses on covering important topics in a systematic way and simultaneously trains students to tackle the entrance examination to improve their expected All India Rank.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", fontSize: "20px"}}>The entire syllabus will be  covered in the short period by the expert professors and it is discussed from the entrance exam point of view. To give the students a feel of the actual exams, they are also provided with the All India Test Series.</li>
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", fontSize: "20px"}}>This Course is designed to cover the entire syllabus of NEET in a short period.</li>
               <br />
               <hr style={{borderTop: "1px solid"}} />
-              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", listStyle: "none"}}>For more details :- kindly contact on mobile number <u style={{color: "black"}}>7900007174</u></li>  
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", listStyle: "none", fontSize: "20px"}}>For more details :- kindly contact on mobile number <u style={{color: "black"}}>7900007174</u></li>  
         </ul>    
      </div>
    </div>
