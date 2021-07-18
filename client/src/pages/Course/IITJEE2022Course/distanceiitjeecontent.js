79a9a4cef2c8394da927176e4a450c2b7ca97bbf
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-18 12:30:46 +0530

    Create distanceiitjeecontent.js

---
+++ client/src/pages/Course/IITJEE2022Course/distanceiitjeecontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DistanceIITJEEContainerCourses from "./DistanceIITJEEContainerCourses.js";
+
+const distanceiitjeecontent=()=> {
+  return (
+    <div >
+      <DistanceIITJEEContainerCourses />
+    </div>
+  );
+}
+
+export default distanceiitjeecontent;
