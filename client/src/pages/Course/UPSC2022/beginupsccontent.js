d978a4ac50c4eb573079f9921317c09377fd90fb
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-18 18:13:16 +0530

    Update and rename basicupsccontent.js to beginupsccontent.js

---
+++ client/src/pages/Course/UPSC2022/beginupsccontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BeginUPSCContainerCourses from "./BeginUPSCContainerCourses.js";
+
+const beginupsccontent=()=> {
+  return (
+    <div >
+      <BeginUPSCContainerCourses />
+    </div>
+  );
+}
+
+export default beginupsccontent;
