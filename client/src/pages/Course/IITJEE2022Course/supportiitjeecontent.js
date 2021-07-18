f9c9b7dd734fbc769abc757df1dfc210c9309d54
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-18 12:11:32 +0530

    Create supportiitjeecontent.js

---
+++ client/src/pages/Course/IITJEE2022Course/supportiitjeecontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SupportIITJEEContainerCourses from "./SupportIITJEEContainerCourses.js";
+
+const supportiitjeecontent=()=> {
+  return (
+    <div >
+      <SupportIITJEEContainerCourses />
+    </div>
+  );
+}
+
+export default supportiitjeecontent;
