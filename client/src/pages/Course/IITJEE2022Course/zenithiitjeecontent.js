efa80159897f8362199b7bc0152e37eb053b7430
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-18 10:55:14 +0530

    Create zenithiitjeecontent.js

---
+++ client/src/pages/Course/IITJEE2022Course/zenithiitjeecontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ZenithIITJEEContainerCourses from "./ZenithIITJEEContainerCourses.js";
+
+const zenithiitjeecontent=()=> {
+  return (
+    <div >
+      <AmpleContainerCourses />
+    </div>
+  );
+}
+
+export default zenithiitjeecontent;
