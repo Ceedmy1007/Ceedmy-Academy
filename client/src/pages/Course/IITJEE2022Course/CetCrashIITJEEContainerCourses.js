5eba862e60c091bee351f2ddba5b29690b92002d
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-18 12:48:25 +0530

    Create CetCrashIITJEEContainerCourses.js

---
+++ client/src/pages/Course/IITJEE2022Course/CetCrashIITJEEContainerCourses.js
@@ -0,0 +1,50 @@
+
+import React from "react";
+
+const CetCrashIITJEEContainerCourses = () => {
+  return (
+      <>
+     
+    <div className="container mt-5 mx-5"
+    >
+      <div className="flexClass">
+        <nav aria-label="breadcrumb">
+         
+          <h2
+            style={{
+              fontFamily: "helvetica",
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: "30px",
+            }}
+          >
+            IIT JEE Courses
+          </h2>
+          <br />
+          <h2
+            style={{
+              fontFamily: "verdana",     
+              textAlign: "center",
+              fontSize: "22px",
+            }}
+          >
+            CET Crash Course
+          </h2>
+        </nav>
+        <br />
+        <ul>
+              <li style={{listStyle: "none", marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", fontSize: "20px"}}>This program really helps students to cover important topics in systematic matter and trains same to tackle the entrance examination level problems in such a way that it helps him to improve the expected state rank.</li>
+              <br />  
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", fontSize: "20px"}}>Discourses designed to cover the entire syllabus of JEE Main and Advance in a short time</li>
+ 
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", fontSize: "20px"}}>Batch schedule is four times a week</li>
+              <br />
+              <hr style={{borderTop: "1px solid"}} />
+              <li style={{marginRight: "15px", marginLeft: "15px", fontFamily: "sans-serif", listStyle: "none", fontSize: "20px"}}>For more details :- kindly contact on mobile number <u style={{color: "black"}}>7900007174</u></li>  
+        </ul>    
+     </div>
+   </div>
+ </>
+  );
+};
+export default CetCrashIITJEEContainerCourses;
