e07e22731292af477027ed31bf605bac1577183c
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-18 12:49:55 +0530

    Create cetcrashiitjeecontent.js

---
+++ client/src/pages/Course/IITJEE2022Course/cetcrashiitjeecontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CetCrashIITJEEContainerCourses from "./CetCrashIITJEEContainerCourses.js";
+
+const cetcrashiitjeecontent=()=> {
+  return (
+    <div >
+      <CetCrashIITJEEContainerCourses />
+    </div>
+  );
+}
+
+export default cetcrashiitjeecontent;
