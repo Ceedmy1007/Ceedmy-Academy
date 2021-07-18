929bd3bb8b01bc3fcdd8ce94eaded11c855a9237
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-18 12:40:57 +0530

    Create crashiitjeecontent.js

---
+++ client/src/pages/Course/IITJEE2022Course/crashiitjeecontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CrashIITJEEContainerCourses from "./CrashIITJEEContainerCourses.js";
+
+const crashiitjeecontent=()=> {
+  return (
+    <div >
+      <CrashIITJEEContainerCourses />
+    </div>
+  );
+}
+
+export default crashiitjeecontent;
