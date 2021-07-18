b9282186861d19a38d24d54a759425b107aa3e8a
Ceedmy1007 <87239700+Ceedmy1007@users.noreply.github.com>
2021-07-18 16:19:48 +0530

    Create basicupsccontent.js

---
+++ client/src/pages/Course/UPSC2022/basicupsccontent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BasicUPSCContainerCourses from "./BasicUPSCContainerCourses.js";
+
+const basicupsccontent=()=> {
+  return (
+    <div >
+      <BasicUPSCContainerCourses />
+    </div>
+  );
+}
+
+export default basicupsccontent;
